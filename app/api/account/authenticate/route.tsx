99c77610d8854951136421071aad65b4d5b295f6
nicolo-rancan-crm <nicolo.rancan@crminf.com>
2023-09-07 18:00:57 +0200

    Save state

--- app/api/account/authenticate/route.tsx
+++ app/api/account/authenticate/route.tsx
@@ -1,8 +1,64 @@
 import { NextResponse } from "next/server";
+import crypto from "crypto";
 import executeQuery from "@/utils/connector";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+const getHashFromDatabase = async (username: string) => {
+  if (!username) return null;
+
+  const uPerms: any = await executeQuery({
+    query: "SELECT * FROM nlogin WHERE last_name = ?",
+    values: [username],
+    dbs: "s64_nLogin",
+  });
+
+  if (uPerms[0] && Object.hasOwn(uPerms[0], "password"))
+    return uPerms[0].password;
+  else return null;
+};
+
+const isValidPassword = (password: string, dbPass: string) => {
+  let hash = crypto.createHash("sha256").update(password).digest("hex");
+  console.log(hash);
+  let hashS = hash + dbPass.split("$")[3];
+  console.log(hashS, dbPass.split("$")[3]);
+  let hashSH = crypto.createHash("sha256").update(hashS).digest("hex");
+  console.log(hashSH);
+
+  if (hashSH === dbPass.split("$")[2]) {
+    return true;
+  }
+
+  return false;
+};
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  if (data && data.username && data.password) {
+    let hash = await getHashFromDatabase(data.username);
+
+    if (hash) {
+      if (isValidPassword(data.password, hash)) {
+      } else {
+        return NextResponse.json({
+          error: true,
+          code: 2,
+        });
+      }
+    } else {
+      return NextResponse.json({
+        error: true,
+        code: 2,
+      });
+    }
+  } else {
+    return NextResponse.json({
+      error: true,
+      code: 1,
+    });
+  }
+
+  /*const { searchParams } = new URL(request.url);
   const username = searchParams.get("username");
   const secret = searchParams.get("secret");
   const type = searchParams.get("type"); // password/token
@@ -10,5 +66,5 @@ export async function GET(request: Request) {
   if (username && secret && type) {
   } else {
     return NextResponse.json({});
-  }
+  }*/
 }
